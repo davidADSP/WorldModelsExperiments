0d45f176bbf4c0be54d48f54892c03e04b83bff9
David Foster <david@applied-data.science>
2018-07-17 15:27:07 +0100

    changed vae training steps

--- doomrnn/env.py
+++ doomrnn/env.py
@@ -1,16 +1,16 @@
 import numpy as np
 import gym
+from doomreal import DoomTakeCoverWrapper
+from doomrnn import DoomCoverRNNEnv
 
 def make_env(env_name, seed=-1, render_mode=False, load_model=True):
   if env_name == 'doomrnn':
     print('making rnn doom environment')
-    from doomrnn import DoomCoverRNNEnv
+    
     env = DoomCoverRNNEnv(render_mode=render_mode, load_model=load_model)
   else:
     print('making real doom environment')
-    from doomreal import DoomTakeCoverWrapper
     print('here before')
-    
     env = DoomTakeCoverWrapper(render_mode=render_mode, load_model=load_model)
     print('here after')
     
